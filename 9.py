3954aa89b13d8f3375c823ed82247f4365657704
João Miguel <joaomiguelmorais1108@gmail.com>
2024-11-05 20:08:21 -0300

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,7 @@
+dicionario = {"jogo":"elden ring", "personagem": "antedeguemom", "filme":"clube da luta"}
+
+dicionario2 = {"comida":"pizza", "fruta":"morango"}
+
+dicionario2.update(dicionario)
+
+print(dicionario2)
