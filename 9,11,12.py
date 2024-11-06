dad6b9087f6c7179f52c1d7ef1bb999da79d0d0b
João Miguel <joaomiguelmorais1108@gmail.com>
2024-11-05 21:28:04 -0300

    Update 9,11,12.py

--- 9,11,12.py
+++ 9,11,12.py
@@ -1,7 +1,7 @@
-dicionario2 = {"fruta1":"laranja" "R$0.5",  "fruta2":"morango" "R$5" }
+dicionario2 = {"laranja": "R$0.5",  "morango" :"R$5" }
 
-preço = {"fruta2":"morango" "R$6"}
+dicionario2["laranja"] = "R$1"
 
-dicionario2.update(preço)
+
 
 print(dicionario2)
