d966883c7f2bf354bd6b4b326b6bf23a8dada58d
João Miguel <joaomiguelmorais1108@gmail.com>
2024-11-05 21:10:00 -0300

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,3 @@
+artistas = {"artista":"terry crews", "filmes e series":"As Branquelas" "Todo mundo odeia o chris", "premios":"Phenom Award 2019" "Audie Award for business/Personal Development 2023"
+
+print(artista)
